9cfac01767f03ea09dbb93d20bec8ce4a4f007cc
in-in <dmin@tuta.io>
2019-05-04 16:01:47 +0300

    feat(hero): add hero component

---
+++ src/components/hero/index.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import st from "./styles.module.scss"
+
+const Hero = () => (
+  <div className={st.hero}>
+    <p>hero block</p>
+  </div>
+)
+
+export default Hero
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Section from "../components/section"
+import Hero from "../components/hero"
 
 const IndexPage = () => (
   <Layout>
     <Header />
-    <Section>hero</Section>
+    <Section>
+      <Hero />
+    </Section>
     <Section>partners</Section>
     <Section>apps</Section>
     <Section>about</Section>
