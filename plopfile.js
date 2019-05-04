340b1860e2fe53a438cde510cb1ed03cf06f78d1
in-in <dmin@tuta.io>
2019-05-04 19:33:57 +0300

    build(plop): add plop config file and templates

---
+++ plopfile.js
@@ -0,0 +1,24 @@
+module.exports = plop => {
+  plop.setGenerator(`component`, {
+    description: `Create a component`,
+    prompts: [
+      {
+        type: `input`,
+        name: `name`,
+        message: `What is your component name?`,
+      },
+    ],
+    actions: [
+      {
+        type: `add`,
+        path: `src/components/{{ lowerCase name }}/index.jsx`,
+        templateFile: `plop-templates/component.jsx.hbs`,
+      },
+      {
+        type: `add`,
+        path: `src/components/{{ lowerCase name }}/styles.module.scss`,
+        templateFile: `plop-templates/component.scss.hbs`,
+      },
+    ],
+  })
+}
