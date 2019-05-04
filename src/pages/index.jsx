e5edf7d18a3433b222417c756ad27cc32c304241
in-in <dmin@tuta.io>
2019-05-04 15:58:37 +0300

    feat(section): add section component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,21 @@
 import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
+import Section from "../components/section"
 
 const IndexPage = () => (
   <Layout>
     <Header />
+    <Section>hero</Section>
+    <Section>partners</Section>
+    <Section>apps</Section>
+    <Section>about</Section>
+    <Section>stats</Section>
+    <Section>blog</Section>
+    <Section>testimonials</Section>
+    <Section>help</Section>
+    <Section>pricing</Section>
+    <Section>newsletter</Section>
   </Layout>
 )
 
