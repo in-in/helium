3ad4154059f244e38ec02492b273e4378732f30e
in-in <dmin@tuta.io>
2019-05-05 22:22:05 +0300

    refactor(Heading): update colors and add strongify function

--- src/components/heading/index.jsx
+++ src/components/heading/index.jsx
@@ -1,11 +1,28 @@
-import React from "react"
+import React, { Fragment } from "react"
 import PropTypes from "prop-types"
 import st from "./styles.module.scss"
+
+const strongify = (line, strongs) => {
+  const result = line.split(` `).map((word, i) => {
+    if (strongs && strongs.some(el => el === i)) {
+      return (
+        // eslint-disable-next-line react/no-array-index-key
+        <strong key={i} className={st.strong}>
+          {word}
+          {` `}
+        </strong>
+      )
+    }
+    // eslint-disable-next-line react/no-array-index-key
+    return <Fragment key={i}>{word} </Fragment>
+  })
+  return result
+}
 
 const Heading = ({ title, subtitle }) => (
   <div className={st.heading}>
     <h1 className={st.title}>{title}</h1>
-    <p className={st.subtitle}>{subtitle}</p>
+    <p className={st.subtitle}>{strongify(subtitle, [2])}</p>
   </div>
 )
 
