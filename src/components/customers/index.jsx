8a8724b811d630fdbaaa638fb2d6948a0af3543a
in-in <dmin@tuta.io>
2019-05-28 14:20:14 +0300

    feat(customers): add customers component

---
+++ src/components/customers/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Container from "../container"
+import st from "./styles.module.scss"
+
+const Customers = () => (
+  <section className={st.customers}>
+    <Container>
+      <p>Customers component</p>
+    </Container>
+  </section>
+)
+
+export default Customers
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import Header from "../components/header"
 import Hero from "../components/hero"
+import Customers from "../components/customers"
 import Software from "../components/software"
 
 const IndexPage = () => (
   <Layout>
     <Header />
     <Hero />
-    <section>partners</section>
+    <Customers />
     <Software />
     <section>about</section>
     <section>stats</section>
