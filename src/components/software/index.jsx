1d1e786f6d7a09c474d758ab629e64a2da8aef51
in-in <dmin@tuta.io>
2019-06-16 21:36:05 +0300

    feat(software): add layout styles

--- src/components/software/index.jsx
+++ src/components/software/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Container from "../container"
 import Heading from "../heading"
+import Text from "../text"
 import st from "./styles.module.scss"
 
 const Software = () => {
@@ -25,9 +26,19 @@ const Software = () => {
   return (
     <section className={st.software}>
       <Container>
-        {data.allSectionHeadingsJson.edges.map(item => (
-          <Heading key={item.node.subtitle} {...item.node} />
-        ))}
+        <div className={st.software__wrapper}>
+          <div className={st.software__desc}>
+            {data.allSectionHeadingsJson.edges.map(item => (
+              <Heading key={item.node.subtitle} {...item.node} />
+            ))}
+            <Text>
+              Brute laoreet efficiendi id his, ea illum nonumes luptatum pro.
+              Usu atqui laudem an, insolens gubergren similique est cu. Et vel
+              modus congue vituperata.
+            </Text>
+          </div>
+          <div className={st.software__showcase}>software__showcase</div>
+        </div>
       </Container>
     </section>
   )
