d91818a6c0b20e9a421ac58c6e1884ef6fad1c3b
in-in <dmin@tuta.io>
2019-06-24 08:31:04 +0300

    feat(button): add all button variants

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,16 +1,30 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Icon from "../icon"
 import st from "./styles.module.scss"
 
-const Button = ({ variant, children }) => (
+const Button = ({ variant, icon, children }) => (
   <button type="button" className={st[`button_${variant}`]}>
     {children}
+    {icon && <Icon filename={icon} classname={st.button__icon} />}
   </button>
 )
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
-  variant: PropTypes.oneOf([`primary`, `secondary`, `danger`]).isRequired,
+  icon: PropTypes.string,
+  variant: PropTypes.oneOf([
+    `primary`,
+    `secondary`,
+    `plain`,
+    `dim`,
+    `bright`,
+    `plain-invert`,
+  ]).isRequired,
+}
+
+Button.defaultProps = {
+  icon: ``,
 }
 
 export default Button
