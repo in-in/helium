f65b59fd3199519b6f58f0050d2874fedfae4e81
in-in <dmin@tuta.io>
2019-08-27 01:18:21 +0300

    chore(eslint): add parser and plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
+	parser: `babel-eslint`,
 	extends: [`airbnb`, `prettier`, `prettier/react`, `airbnb/hooks`],
+	plugins: [`prettier`],
 	rules: {
 		quotes: [`error`, `backtick`],
+		"prettier/prettier": `error`,
 		"react/jsx-props-no-spreading": [`error`, { exceptions: [`Heading`] }],
 	},
 }
