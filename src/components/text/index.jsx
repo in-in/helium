13745e3b9e7a154320b0b582ac1e16c5c6a07218
in-in <dmin@tuta.io>
2019-10-24 10:52:13 +0300

    refactor(text): add default tag name

--- src/components/text/index.jsx
+++ src/components/text/index.jsx
@@ -2,17 +2,20 @@ import React from "react"
 import PropTypes from "prop-types"
 import st from "./styles.module.scss"
 
-const Text = ({ classname, children }) => (
-	<p className={classname || st.text}>{children}</p>
-)
+const Text = ({ classname, children, as }) => {
+	const Component = as
+	return <Component className={classname || st.text}>{children}</Component>
+}
 
 Text.propTypes = {
 	children: PropTypes.node.isRequired,
 	classname: PropTypes.string,
+	as: PropTypes.elementType,
 }
 
 Text.defaultProps = {
 	classname: ``,
+	as: `p`,
 }
 
 export default Text
