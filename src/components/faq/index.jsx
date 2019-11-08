ae593ecb45c8aef0501e2c787f9f109ee043691e
in-in <dmin@tuta.io>
2019-11-08 14:11:47 +0300

    feat(faq): add faq component

---
+++ src/components/faq/index.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Text from "../text"
+import Button from "../button"
+import st from "./styles.module.scss"
+
+const Faq = ({ question, answer, link, accordionHeader }) => {
+	return (
+		<div className={st.faq}>
+			{!accordionHeader && <h3 className={st.faq__question}>{question}</h3>}
+			<Text classname={st.faq__answer}>{answer}</Text>
+			<Button variant="dim" classname={st.faq__link}>
+				Go to documentation {link}
+			</Button>
+		</div>
+	)
+}
+
+Faq.propTypes = {
+	question: PropTypes.string.isRequired,
+	answer: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+	accordionHeader: PropTypes.bool,
+}
+
+Faq.defaultProps = {
+	accordionHeader: false,
+}
+
+export default Faq
