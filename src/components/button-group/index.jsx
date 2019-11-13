35836c5d754e30f403265240101240959dd9c350
in-in <dmin@tuta.io>
2019-11-13 15:53:40 +0300

    refactor(button-group): add parentClass prop

--- src/components/button-group/index.jsx
+++ src/components/button-group/index.jsx
@@ -2,12 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import st from "./styles.module.scss"
 
-const ButtonGroup = ({ children }) => (
-	<div className={st[`button-group`]}>{children}</div>
-)
+const ButtonGroup = ({ parentClass, children }) => {
+	const parentClassList = parentClass ? ` ${parentClass}` : parentClass
+
+	return (
+		<div className={`${st[`button-group`]}${parentClassList}`}>{children}</div>
+	)
+}
 
 ButtonGroup.propTypes = {
+	parentClass: PropTypes.string,
 	children: PropTypes.node.isRequired,
+}
+
+ButtonGroup.defaultProps = {
+	parentClass: ``,
 }
 
 export default ButtonGroup
