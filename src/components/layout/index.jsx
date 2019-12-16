7be2c271071627aa421cf8f6dac1951cbb328160
in-in <dmin@tuta.io>
2019-12-16 11:15:34 +0300

    refactor(fonts): add preload option for regular fonts

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
+import RobotoRegular from '../../fonts/roboto-v19-latin-regular.woff2';
+import QuicksandRegular from '../../fonts/quicksand-v9-latin-regular.woff2';
 import '../../styles/index.scss';
 
 const Layout = ({ children }) => (
@@ -14,12 +16,42 @@ const Layout = ({ children }) => (
 				designers as well as project managers who need a beautiful tool
 				that makes their work really pleasant."
 			/>
+			<link
+				rel="preload"
+				href={QuicksandRegular}
+				as="font"
+				type="font/woff2"
+				crossOrigin=""
+			/>
+			<link
+				rel="preload"
+				href={RobotoRegular}
+				as="font"
+				type="font/woff2"
+				crossOrigin=""
+			/>
 			<style>
 				{`
 				svg {
 					width: 1em;
 					height: 1em;
 				}
+
+				@font-face {
+					font-family: Quicksand;
+					font-weight: 400;
+					font-style: normal;
+					font-display: swap;
+					src: url(${QuicksandRegular}) format("woff2")
+					}
+
+				@font-face {
+					font-family: Roboto;
+					font-weight: 400;
+					font-style: normal;
+					font-display: swap;
+					src: url(${RobotoRegular}) format("woff2")
+					}
 			`}
 
 			</style>
