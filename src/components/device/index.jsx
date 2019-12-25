faf3b91b77283363a63b70e9841da6b890374c4f
in-in <dmin@tuta.io>
2019-12-25 21:04:53 +0300

    refactor(device): delete unnecessary picture element

--- src/components/device/index.jsx
+++ src/components/device/index.jsx
@@ -1,52 +1,22 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import Icon from '../icon';
-import Picture from '../picture';
 import st from './styles.module.scss';
 
-const Device = () => {
-	const data = useStaticQuery(graphql`
-		query {
-			laptop: allDeviceJson(filter: { device: { eq: "laptop" } }) {
-				nodes {
-					...device
-				}
-			}
-			phone: allDeviceJson(filter: { device: { eq: "phone" } }) {
-				nodes {
-					...device
-				}
-			}
-			tablet: allDeviceJson(filter: { device: { eq: "tablet" } }) {
-				nodes {
-					...device
-				}
-			}
+const devices = ['phone', 'laptop', 'tablet'];
+
+const Device = () => (
+	<ul className={st.device}>
+		{
+			devices.map((name) => (
+				<li className={st[`device__item_${name}`]}>
+					<Icon filename={`device/${name}`} classname={st.device__mockup} />
+					<Icon
+						filename={`device/device_${name}`}
+						classname={st[`device__picture_${name}`]}
+					/>
+				</li>
+			))
 		}
-	`);
-
-	return (
-		<div className={st.device}>
-			<div className={st.device__item_phone}>
-				<Icon filename="device/phone" classname={st.device__mockup} />
-				<Picture data={data.phone.nodes[0].images} classname={st.device__picture_phone} />
-			</div>
-			<div className={st.device__item_laptop}>
-				<Icon filename="device/laptop" classname={st.device__mockup} />
-				<Picture
-					data={data.laptop.nodes[0].images}
-					classname={st.device__picture_laptop}
-				/>
-			</div>
-			<div className={st.device__item_tablet}>
-				<Icon filename="device/tablet" classname={st.device__mockup} />
-				<Picture
-					data={data.tablet.nodes[0].images}
-					classname={st.device__picture_tablet}
-				/>
-			</div>
-		</div>
-	);
-};
-
+	</ul>
+);
 export default Device;
