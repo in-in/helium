8334b609f0c64bc2e9d9e6e002599dcd20a53e61
in-in <dmin@tuta.io>
2020-01-25 15:35:25 +0300

    refactor(logo): add link text to improve a11y

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import st from './styles.module.scss';
+import { useStaticQuery, graphql } from 'gatsby';
 import Icon from '../icon';
+import st from './styles.module.scss';
 
-const Logo = () => (
-	<a href="/" className={st.logo}>
-		<Icon filename="logo" parentClass={st.logo__icon} />
-	</a>
-);
+const Logo = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			site {
+				siteMetadata {
+					title
+				}
+			}
+		}
+`);
+
+	return (
+		<a
+			href="/"
+			className={st.logo}
+			aria-label={`${data.site.siteMetadata.title} Homepage`}
+		>
+			<Icon filename="logo" parentClass={st.logo__icon} />
+		</a>
+	);
+};
 
 export default Logo;
